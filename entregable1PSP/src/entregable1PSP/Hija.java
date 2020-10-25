d7c29faecf37c1c174f252e4467f7ac61e1244d9
Usuario <Usuario@DESKTOP-HQHKU1L>
2020-10-25 13:12:10 +0100

    comentarios

--- entregable1PSP/src/entregable1PSP/Hija.java
+++ entregable1PSP/src/entregable1PSP/Hija.java
@@ -14,22 +14,37 @@ public class Hija {
 		int n = 1;
 		int n2 = 0;
 		String entrada;
-		boolean fecha = false;
+		// variables booleanas creadas para mejorar el control durante el proceso
+		boolean fecha = false,mesas=false,correcto;
+		/*
+		 * correcto: la uso para que el programa sepa que se ha introducido una opcion correcta, de no ser así, se enviria un mensaje.
+		 * mesa: la uso para que el programa sepa que ya se ha introducido el primer comando y ahora debe preguntar por el numero de mesas.
+		 * fecha: la uso para que el programa sepa ya se ha introducido un numero de mesas soportado y ahora debe preguntar por una fecha de reserva.
+		 * */
+		//Aqui programo el input de la clase para que recoja los datos que la clase Padre le envia
 		InputStreamReader isr = new InputStreamReader(System.in);
 		BufferedReader br = new BufferedReader(isr);
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/mm/yyyy");
 		Date f = null;
-		boolean correcto;
+		//Inicio de la clase
 		System.out.println(
 				"Bienvenido a central de reservas del restaurante Delicias de Pablo \n" + "¿En qué puedo ayudarle?");
+		//Linea informativa para que el usuario sepa como empezar a usar el programa.
+		System.out.println("Teclee ,ayuda, para ver los comandos disponibles actualmente.");
+		//Creacion de un bucle infinito para que la clase no muera hasta que n2 sea mayor o igual que n
 		while (n > n2) {
+			//Si hay datos enviados a esta clase, se ejecuta el if
 			if (br.ready()) {
 				correcto = false;
+				//Leo la informacion que ha introducido la clase Padre
 				entrada = br.readLine();
 				if (entrada.equalsIgnoreCase("Reservar mesa")) {
+					fecha=false;//Cambio la variable fecha a false para que el proceso de reserva se reinicie si vuelves a introducir este comando.
 					System.out.println("Perfecto, ¿cuántas personas asistirán a la comida?");
 					correcto = true;
+					mesas=true;
 				}
+				//Compruebo si ya debo recoger los datos de la fecha con el booleano fecha
 				if(fecha) {
 					try {
 						f = sdf.parse(entrada);
@@ -39,8 +54,10 @@ public class Hija {
 					}catch(ParseException e) {
 						System.out.println("Fecha mal introducida");
 					}
-				}
+				}else if(mesas) {
+					//Compruebo si el dato introducido es un numero entero
 				try {
+					//Si lo es, recojo el dato y dependiendo de el numero introducido, muestro un mesaje u otro.
 					int i = Integer.parseInt(entrada);
 					int nMesas = calcNumeMesas(i);
 					if (nMesas == -1) {
@@ -54,9 +71,14 @@ public class Hija {
 						fecha = true;
 					}
 					correcto = true;
-
+					//Si el dato no es un numero recojo la excepcion generada por el metodo parseInt y dejo que el programa siga su ejecución
 				} catch (NumberFormatException e) {
 					// TODO: handle exception
+				}
+				}
+				if(entrada.equalsIgnoreCase("ayuda")) {
+					System.out.println("-Reservar mesa:  Comienza el proceso para reservar mesa");
+					correcto=true;
 				}
 
 				if (!correcto) {
@@ -66,7 +88,7 @@ public class Hija {
 
 		}
 	}
-
+//Metodo que maneja el dato del numero de personas para devolver el numero de mesas reservadas
 	private static int calcNumeMesas(int i) {
 		// TODO Auto-generated method stub
 		int num = 1;
--- entregable1PSP/src/entregable1PSP/Padre.java
+++ entregable1PSP/src/entregable1PSP/Padre.java
@@ -11,23 +11,27 @@ public class Padre {
 
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
+		//En estas dos primeras líneas ejecuto la clase hija, la primera línea se utiliza para compilar la clase, y con la segunda la ejecuto
 		Process proceso = Runtime.getRuntime().exec("javac -cp src src/entregable1PSP/Hija.java");
 		Process pr = Runtime.getRuntime().exec("java -cp src entregable1PSP/Hija");
+		//Aqui programo el output de la clase Hija para que envíe los datos escritos en la clase Hija a la clase Padre
 		InputStream is = pr.getInputStream();
+		//Aqui programo el input de la clase  para que los datos que se escriban en la consola de la clase Padre vayan al input de la clase Hija
 		OutputStream os = pr.getOutputStream();
 		String line;
+		//Compruebo que la clase Hija esta en ejecucion
 		while (pr.isAlive()) {
-			//System.out.println("inicio bucle");
+			//Creacion de un buffer para comunicar las dos clases
 			byte[] buffer = new byte[4000];
 			int in = System.in.available();
-			//Compruebo si hay datos en la consola
+			//Compruebo si hay datos de la clase Hija por imprimir
 			//bucle para imprimir los mensajes de la clase hija
 			int no = is.available();
 			if(no>0) {
 				int n = is.read(buffer, 0, Math.min(no, buffer.length));
 				System.out.println(new String(buffer, 0,n));
 			}
-			
+			//Compruebo si hay informacion por enviar a la clase Hija
 			int ni = System.in.available();
 			if (ni > 0) {
 				//si existe información se envía al proceso hijo
