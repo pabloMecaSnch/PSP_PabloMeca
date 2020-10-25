1f6041d314f71bc1fd191a6f272948ff71321bb1
Usuario <Usuario@DESKTOP-HQHKU1L>
2020-10-25 12:30:27 +0100

    Hija 1.0

--- entregable1PSP/src/entregable1PSP/Hija.java
+++ entregable1PSP/src/entregable1PSP/Hija.java
@@ -3,6 +3,9 @@ package entregable1PSP;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class Hija {
 
@@ -11,16 +14,72 @@ public class Hija {
 		int n = 1;
 		int n2 = 0;
 		String entrada;
+		boolean fecha = false;
 		InputStreamReader isr = new InputStreamReader(System.in);
 		BufferedReader br = new BufferedReader(isr);
-		System.out.println("hola");
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/mm/yyyy");
+		Date f = null;
+		boolean correcto;
+		System.out.println(
+				"Bienvenido a central de reservas del restaurante Delicias de Pablo \n" + "¿En qué puedo ayudarle?");
 		while (n > n2) {
 			if (br.ready()) {
+				correcto = false;
 				entrada = br.readLine();
-				if (entrada.equals("a")) {
-					System.out.println("has escrito una a");
+				if (entrada.equalsIgnoreCase("Reservar mesa")) {
+					System.out.println("Perfecto, ¿cuántas personas asistirán a la comida?");
+					correcto = true;
+				}
+				if(fecha) {
+					try {
+						f = sdf.parse(entrada);
+						System.out.println("fecha: "+sdf.format(f).toString()+"\nLa reserva se ha realizado con éxito, ¡muchas gracias!");
+						n2=2;
+						correcto= true;
+					}catch(ParseException e) {
+						System.out.println("Fecha mal introducida");
+					}
+				}
+				try {
+					int i = Integer.parseInt(entrada);
+					int nMesas = calcNumeMesas(i);
+					if (nMesas == -1) {
+						System.out.println("Lo sentimos, no podemos hacer reservas tan grandes");
+					}else if(nMesas==0) {
+						System.out.println("Lo sentimos, no podemos hacer reservas para 0 personas o menos.");
+					}else {
+						System.out.println("De acuerdo, para una comida con " + i + " personas, hemos reservado "
+								+ nMesas + " mesas.");
+						System.out.println("Introduzca una fecha con el siguiente formato: dd/mm/yyyy");
+						fecha = true;
+					}
+					correcto = true;
+
+				} catch (NumberFormatException e) {
+					// TODO: handle exception
+				}
+
+				if (!correcto) {
+					System.out.println("lo siento, no te entendido");
 				}
 			}
+
 		}
 	}
+
+	private static int calcNumeMesas(int i) {
+		// TODO Auto-generated method stub
+		int num = 1;
+		if (i >= 6 && i <= 12) {
+			num = 2;
+		}
+		if (i > 12) {
+			num = -1;
+		}
+		if(i<=0 ) {
+			num=0;
+		}
+		return num;
+	}
+
 }
--- entregable1PSP/src/entregable1PSP/Padre.java
+++ entregable1PSP/src/entregable1PSP/Padre.java
@@ -14,18 +14,24 @@ public class Padre {
 		Process proceso = Runtime.getRuntime().exec("javac -cp src src/entregable1PSP/Hija.java");
 		Process pr = Runtime.getRuntime().exec("java -cp src entregable1PSP/Hija");
 		InputStream is = pr.getInputStream();
-		InputStreamReader isr = new InputStreamReader(is);
-		BufferedReader br = new BufferedReader(isr);
 		OutputStream os = pr.getOutputStream();
 		String line;
 		while (pr.isAlive()) {
-			System.out.println("inicio bucle");
+			//System.out.println("inicio bucle");
 			byte[] buffer = new byte[4000];
 			int in = System.in.available();
 			//Compruebo si hay datos en la consola
-			if(in > 0) {
-				//si los hay, mando los datos a la clase hija
-				int n = System.in.read(buffer, 0, Math.min(in, buffer.length));
+			//bucle para imprimir los mensajes de la clase hija
+			int no = is.available();
+			if(no>0) {
+				int n = is.read(buffer, 0, Math.min(no, buffer.length));
+				System.out.println(new String(buffer, 0,n));
+			}
+			
+			int ni = System.in.available();
+			if (ni > 0) {
+				//si existe información se envía al proceso hijo
+				int n = System.in.read(buffer, 0, Math.min(ni, buffer.length));
 				os.write(buffer, 0, n);
 				os.flush();
 			}
@@ -36,13 +42,6 @@ public class Padre {
 				e.printStackTrace();
 				Thread.currentThread().interrupt();
 			}
-			//bucle para imprimir los mensajes de la clase hija
-			
-			while ((line = br.readLine()) != null) {
-				System.out.println("bucle");
-				System.out.println(line);
-			}
-			System.out.println("fin bucle");
 		}
 		System.out.println(pr.exitValue());
 	}
