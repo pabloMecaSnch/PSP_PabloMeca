ac5992524582c4073112fd4cfa38ed6ed14606f5
Pablo <Pablo@DESKTOP-6IIT31R>
2020-11-29 19:18:43 +0100

    versión final (Inclusión de JavaDoc)

--- entregable2/doc/type-search-index.js
+++ entregable2/doc/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"entregable2","l":"Persona"}]
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"entregable2","l":"Coworking"},{"p":"entregable2","l":"Ordenador"},{"p":"entregable2","l":"Persona"},{"p":"entregable2","l":"Tarjeta"}]
