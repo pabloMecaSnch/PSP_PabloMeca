e7c93791f3fee0aafc74066e29cd35fd10409468
Pablo <Pablo@DESKTOP-6IIT31R>
2021-01-26 14:46:43 +0100

    web

---
+++ pspWeb/src/test/java/com/pspWeb/pspWeb/PspWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pspWeb.pspWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PspWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
