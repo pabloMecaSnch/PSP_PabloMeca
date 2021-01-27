8f73f13f1fcd81cb9cfc5477d43c9adcc946a7a1
Pablo <Pablo@DESKTOP-6IIT31R>
2021-01-27 16:33:03 +0100

    pspro

---
+++ pspro/src/main/java/com/pspro/xml/school/StudentAddressResponse.java
@@ -0,0 +1,71 @@
+//
+// Este archivo ha sido generado por la arquitectura JavaTM para la implantación de la referencia de enlace (JAXB) XML v2.2.7 
+// Visite <a href="http://java.sun.com/xml/jaxb">http://java.sun.com/xml/jaxb</a> 
+// Todas las modificaciones realizadas en este archivo se perderán si se vuelve a compilar el esquema de origen. 
+// Generado el: 2021.01.27 a las 02:39:40 PM CET 
+//
+
+
+package com.pspro.xml.school;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Clase Java para anonymous complex type.
+ * 
+ * <p>El siguiente fragmento de esquema especifica el contenido que se espera que haya en esta clase.
+ * 
+ * <pre>
+ * &lt;complexType>
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;sequence>
+ *         &lt;element name="address" type="{http://www.w3.org/2001/XMLSchema}string"/>
+ *       &lt;/sequence>
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "", propOrder = {
+    "address"
+})
+@XmlRootElement(name = "StudentAddressResponse")
+public class StudentAddressResponse {
+
+    @XmlElement(required = true)
+    protected String address;
+
+    /**
+     * Obtiene el valor de la propiedad address.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getAddress() {
+        return address;
+    }
+
+    /**
+     * Define el valor de la propiedad address.
+     * 
+     * @param student.getAddress
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setAddress(Student student) {
+        this.address = student.getAddress();
+    }
+
+}
