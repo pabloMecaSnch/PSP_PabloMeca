f57da25d9e712f5fb159d83bbc9be265e0469fb5
pabloMecaSnch <60503126+pabloMecaSnch@users.noreply.github.com>
2021-02-09 14:08:37 +0100

    más archivos

---
+++ entregableRecu/src/main/java/com/entregableRecu/webservice/EntregableRecuApplication.java
@@ -0,0 +1,13 @@
+package com.entregableRecu.webservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EntregableRecuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EntregableRecuApplication.class, args);
+	}
+
+}
---
+++ entregableRecu/src/test/java/com/entregableRecu/webservice/EntregableRecuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.entregableRecu.webservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EntregableRecuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
