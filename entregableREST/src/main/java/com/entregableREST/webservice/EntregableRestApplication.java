80db084514f5bf1e7bc622d09248ab58bb39aa43
pabloMecaSnch <60503126+pabloMecaSnch@users.noreply.github.com>
2021-02-17 19:19:38 +0100

    Entregable rest

---
+++ entregableREST/src/main/java/com/entregableREST/webservice/EntregableRestApplication.java
@@ -0,0 +1,13 @@
+package com.entregableREST.webservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EntregableRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EntregableRestApplication.class, args);
+	}
+
+}
